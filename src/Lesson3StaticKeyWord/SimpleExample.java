1d48324fbe8c063f8d213280aa5e6453057ba78d
Sujan Raj Shrestha <sujanrajs.me@gmail.com>
2020-11-30 20:32:34 +0200

    staticExample

--- src/Lesson3StaticKeyWord/SimpleExample.java
+++ src/Lesson3StaticKeyWord/SimpleExample.java
@@ -1,4 +1,17 @@
 package Lesson3StaticKeyWord;
 
-public class SimpleExample {
+public class SimpleExample{
+    // This is a static method
+    static void myMethod()
+    {
+        System.out.println("myMethod");
+    }
+
+    public static void main(String[] args)
+    {
+        /* You can see that we are calling this
+         * method without creating any object.
+         */
+        myMethod();
+    }
 }
